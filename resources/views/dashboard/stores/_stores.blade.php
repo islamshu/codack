e291985d23347f929fb4a08842e0012978cc2e93
islamshu12 <islamshu12@gmail.com>
2022-08-10 14:00:01 +0300

    d

--- resources/views/dashboard/stores/_stores.blade.php
+++ resources/views/dashboard/stores/_stores.blade.php
@@ -1,10 +1,13 @@
 <tr>
     <td>{{ $key }}</td>
-    <td><img src="{{ asset('uploads/'.$store->image) }}" width="100" height="70" alt=""> </td>
+    <td><img src="{{ asset('uploads/'.$store->image) }}" width="50" height="30" alt=""> </td>
     <td>{{ $store->title }} </td>
     <td>0 </td>
     <td>0</td>
+    <td>{{ @$store->user->hasRole('Admin') ?'الادارة' : @$store->user->name }}</td>
     <td>
+        @if(auth()->user()->hasRole('Admin'))
+
         <button class="btn btn-info" data-toggle="modal" data-target="#myModal4"
         onclick="make('{{ $store->id }}')"><i class="fa fa-edit"></i></button>
         <form style="display: inline" action="{{ route('stores.destroy',$store->id) }}" method="post">
@@ -12,5 +15,8 @@
             <button type="submit"  class="btn btn-danger"><i
                     class="fa fa-trash"></i></button>
         </form>
+        @else
+        _
+        @endif
     </td>
 </tr>
