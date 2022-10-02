096f32cdd3ddb481390c452ba5809e7e7c1e8c93
islamshu12 <islamshu12@gmail.com>
2022-10-02 18:50:45 +0300

    edit

--- resources/views/dashboard/wallet/orders.blade.php
+++ resources/views/dashboard/wallet/orders.blade.php
@@ -33,4 +33,30 @@
     </tbody>
 </table>
 
-
+@if (get_lang() == 'ar')
+<script>
+    $('table').DataTable({
+        "initComplete": function(settings, json) {
+            $("table").wrap("<div style='overflow:auto; width:100%;position:relative;'></div>");
+        },
+        "language": {
+            "sProcessing": "جارٍ التحميل...",
+            "sLengthMenu": "أظهر _MENU_ مدخلات",
+            "sZeroRecords": "لم يعثر على أية سجلات",
+            "sInfo": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخلات",
+            "sInfoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
+            "sInfoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
+            "sInfoPostFix": "",
+            "sSearch": "ابحث:",
+            "sUrl": "",
+            "oPaginate": {
+                "sFirst": "الأول",
+                "sPrevious": "السابق",
+                "sNext": "التالي",
+                "sLast": "الأخير"
+            }
+        }
+    });
+  
+</script>
+@endif
