f61acb2e7f8e913cb016bc41704ec1dff2aa5cfe
islamshu12 <islamshu12@gmail.com>
2022-10-02 19:09:36 +0300

    d

--- resources/views/dashboard/change/edit.blade.php
+++ resources/views/dashboard/change/edit.blade.php
@@ -1,57 +1,60 @@
-    <div class="clear-fix"></div>
+@extends('layouts.backend_new')
+@section('content')
+<div class="clear-fix"></div>
 
-    <label for="" class="d-block fs-6 fw-bold">بيانات البنك الحالية</label>
+<label for="" class="d-block fs-6 fw-bold">بيانات البنك الحالية</label>
 
-    <div class="input-item">
-      <label for="">اسم البنك الحالي :<span class="text-danger">*</span></label>
-      <input type="text" class="form-control" value="{{@ $change->famous->bank->bank_name }}" readonly id="email">
-    </div>
-
-
-    <div class="input-item">
-      <label for="">رقم الحساب الحالي :<span class="text-danger">*</span></label>
-      <input type="number" class="form-control" required value="{{@ $change->famous->bank->account_name }}" readonly id="email">
-    </div>
+<div class="input-item">
+  <label for="">اسم البنك الحالي :<span class="text-danger">*</span></label>
+  <input type="text" class="form-control" value="{{@ $change->famous->bank->bank_name }}" readonly id="email">
+</div>
 
 
-    <div class="input-item">
-      <label for="">الاسم بالبنك الحالي :<span class="text-danger">*</span></label>
-      <input type="text" class="form-control" required value="{{@ $change->famous->bank->account_nubmer }}" readonly id="email">
-    </div>
+<div class="input-item">
+  <label for="">رقم الحساب الحالي :<span class="text-danger">*</span></label>
+  <input type="number" class="form-control" required value="{{@ $change->famous->bank->account_name }}" readonly id="email">
+</div>
 
 
-    <div class="clear-fix"></div>
+<div class="input-item">
+  <label for="">الاسم بالبنك الحالي :<span class="text-danger">*</span></label>
+  <input type="text" class="form-control" required value="{{@ $change->famous->bank->account_nubmer }}" readonly id="email">
+</div>
 
 
-    <label class="d-block fs-6 fw-bold mt-3 mt-2">تغير البيانات الحالية</label>
-    <form action="{{ route('update_back_info_by_admin') }}" method="post">
-        @csrf
-        <input type="hidden" name="id" value="{{ $change->id }}">
-
-        <input type="hidden" name="famous_id" value="{{ $change->famous_id }}">
-    <div class="input-item">
-      <label for="">اسم البنك الجديد :<span class="text-danger">*</span></label>
-      <input type="text" class="form-control" value="{{$change->bank_name}}" name="bank_name" >
-
-    </div>
+<div class="clear-fix"></div>
 
 
-    <div class="input-item">
-      <label for="">رقم الحساب الجديد :<span class="text-danger">*</span></label>
-      <input type="number" class="form-control" required  value="{{$change->account_number}}" name="account_name" >    </div>
+<label class="d-block fs-6 fw-bold mt-3 mt-2">تغير البيانات الحالية</label>
+<form action="{{ route('update_back_info_by_admin') }}" method="post">
+    @csrf
+    <input type="hidden" name="id" value="{{ $change->id }}">
+
+    <input type="hidden" name="famous_id" value="{{ $change->famous_id }}">
+<div class="input-item">
+  <label for="">اسم البنك الجديد :<span class="text-danger">*</span></label>
+  <input type="text" class="form-control" value="{{$change->bank_name}}" name="bank_name" >
+
+</div>
 
 
-    <div class="input-item">
-      <label for=""> الاسم بالبنك الجديد    :<span class="text-danger">*</span></label>
-      <input type="text" class="form-control" required value="{{$change->account_name}}" name="account_number">
-    </div>
-
-    <div class="clear-fix"></div>
+<div class="input-item">
+  <label for="">رقم الحساب الجديد :<span class="text-danger">*</span></label>
+  <input type="number" class="form-control" required  value="{{$change->account_number}}" name="account_name" >    </div>
 
 
+<div class="input-item">
+  <label for=""> الاسم بالبنك الجديد    :<span class="text-danger">*</span></label>
+  <input type="text" class="form-control" required value="{{$change->account_name}}" name="account_number">
+</div>
 
-    <div class="input-item">
-      <button  type="submit" class="btn text-end add-store">تعديل</button>
-    </div>
+<div class="clear-fix"></div>
 
-  </form>
+
+
+<div class="input-item">
+  <button   type="submit" class="btn btn-primary text-end add-store">تعديل</button>
+</div>
+
+</form>
+@endsection
