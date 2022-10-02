89f27e2547c83484b620f28ae725927cf6a99236
islamshu12 <islamshu12@gmail.com>
2022-10-02 18:51:26 +0300

    edit

--- resources/views/dashboard/wallet/orders.blade.php
+++ resources/views/dashboard/wallet/orders.blade.php
@@ -1,4 +1,4 @@
-<table id="example" class="display compact" style="width:100%" >
+<table class="display compact" style="width:100%" id="storestable">
     <thead>
         <tr>
             <th>#</th>
@@ -33,4 +33,30 @@
     </tbody>
 </table>
 
-
+@if (get_lang() == 'ar')
+<script>
+    $('#storestable').DataTable({
+        "initComplete": function(settings, json) {
+            $("#storestable").wrap("<div style='overflow:auto; width:100%;position:relative;'></div>");
+        },
+        "language": {
+            "sProcessing": "جارٍ التحميل...",
+            "sLengthMenu": "أظهر _MENU_ مدخلات",
+            "sZeroRecords": "لم يعثر على أية سجلات",
+            "sInfo": "إظهار _START_ إلى _END_ من أصل _TOTAL_ مدخلات",
+            "sInfoEmpty": "يعرض 0 إلى 0 من أصل 0 سجل",
+            "sInfoFiltered": "(منتقاة من مجموع _MAX_ مُدخل)",
+            "sInfoPostFix": "",
+            "sSearch": "ابحث:",
+            "sUrl": "",
+            "oPaginate": {
+                "sFirst": "الأول",
+                "sPrevious": "السابق",
+                "sNext": "التالي",
+                "sLast": "الأخير"
+            }
+        }
+    });
+  
+</script>
+@endif
